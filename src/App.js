5e06320868e0b0e713f29195399cc6abac30b59a
Mustafa Abdelrahman <mustafausama@outlook.com>
2020-03-22 22:53:55 +0300

    List Items are ready and can be checked or unchecked

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './Components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Clean the room',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Eat launch',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Do the laundry',
+        completed: false
+      }
+    ]
+  }
+  markComplete = (id) => {
+	this.setState({ todos: this.state.todos.map(todo => {
+		if(todo.id === id) {
+			todo.completed = !todo.completed;
+		}
+		return todo;
+	})});
+  }
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
 }
 
 export default App;
